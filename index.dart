15b82219312c578fca95554764fd19fe2d73a8aa
Mohammed <mohammedashour742@gmail.com>
2025-02-21 08:14:42 -0800

    adding circle path function

--- index.dart
+++ index.dart
@@ -3,7 +3,12 @@ import 'dart:math';
 void main(){
     print("Welcome to my simple clculator");
     print("The path of the rectangle is ${getRectanglePath(5, 7)}");
+    print("The path of the circle is ${getCirclePath(3.5)}");
 }
 double getRectanglePath(double len, double wid){
     return 2 * (len + wid);
+}
+
+double getCirclePath(double radius){
+    return 2*pi*radius;
 }
