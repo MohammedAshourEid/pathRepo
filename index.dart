db19a6a5be8314f4fbc5e8e71b0d08bf94905efb
Mohammed <mohammedashour742@gmail.com>
2025-02-21 08:34:57 -0800

    adding end message

--- index.dart
+++ index.dart
@@ -1,14 +1,15 @@
 import 'dart:math';
 
 void main(){
-    print("Welcome to my simple clculator");
+    print("Welcome to my simple calculator");
     print("The path of the rectangle is ${getRectanglePath(5, 7)}");
     print("The path of the circle is ${getCirclePath(3.5)}");
+    print("End og calculator");
 }
 double getRectanglePath(double len, double wid){
     return 2 * (len + wid);
 }
 
-double getCirclePath(double radius){
-    return 2*pi*radius;
+double getCirclePath(double radius) {
+    return 2 * pi * radius;
 }
