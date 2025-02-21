92b5e89bc5bbb5fc35eb3b96f69747dd03a3aa1a
Mohammed <mohammedashour742@gmail.com>
2025-02-21 08:38:51 -0800

    no more

--- index.dart
+++ index.dart
@@ -1,7 +1,7 @@
 import 'dart:math';
 
 void main(){
-    print("Welcome to my simple clculator");
+    print("Welcome to my simple calculator");
     print("The path of the rectangle is ${getRectanglePath(5, 7)}");
     print("The path of the circle is ${getCirclePath(3.5)}");
 }
