e98f2d25c9cdd0650172b0e39a751a52f06fcb88
Mohammed <mohammedashour742@gmail.com>
2025-02-21 08:12:54 -0800

    addaing rectangle path function

--- index.dart
+++ index.dart
@@ -2,5 +2,8 @@ import 'dart:math';
 
 void main(){
     print("Welcome to my simple clculator");
+    print("The path of the rectangle is ${getRectanglePath(5, 7)}");
 }
-
+double getRectanglePath(double len, double wid){
+    return 2 * (len + wid);
+}
